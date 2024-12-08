63eda7c39c5b595871b03194e531429a601d38ab
andreainzaghi <andrea3aprile99@gmail.com>
2024-12-08 17:47:11 +0100

    d

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+console.log('Alias configurati:', {
+  '@': path.resolve(__dirname, './src'),
+});
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // L'alias "@" punta a "src"
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });

b542f1ff6c8a51c7b92e8bd416849549e657e990
andreainzaghi <andrea3aprile99@gmail.com>
2024-12-08 17:01:53 +0100

    d

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Alias per accedere facilmente alla cartella src
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  optimizeDeps: {
-    exclude: ['lucide-react'], // Escludi lucide-react dall'ottimizzazione
-  },
   build: {
-    outDir: 'dist', // Directory di output per la build
-    sourcemap: false, // Disabilita le mappe dei sorgenti per ridurre la dimensione del build finale (modifica in base alle necessità)
-    assetsInlineLimit: 4096, // Inline assets fino a 4 KB (opzionale)
+    outDir: 'dist',
   },
   server: {
-    host: '0.0.0.0', // Consenti l'accesso da altri dispositivi nella rete
-    port: 3000, // Porta del server di sviluppo
-    open: true, // Apri automaticamente il browser all'avvio
-    strictPort: true, // Solleva un errore se la porta è già in uso
+    host: true,
+    port: 3000,
+    open: true,
   },
-  base: '/', // Percorso base per il progetto, importante per il deploy su server con SPA
+  base: '/', // Mantieni "/" per il deploy su Vercel
 });

fb99f1426c459dfd29214db5773e8181f69fdc72
andreainzaghi <andrea3aprile99@gmail.com>
2024-12-08 15:20:16 +0100

    y

--- types/aframe.d.ts
+++ types/aframe.d.ts
@@ -1,11 +1,12 @@
 declare namespace JSX {
     interface IntrinsicElements {
-      'a-scene': any;
-      'a-box': any;
-      'a-light': any;
-      'a-plane': any;
-      'a-camera': any;
-      // Aggiungi altri elementi di A-Frame se necessario
+      'a-scene': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'a-entity': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'a-box': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'a-light': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'a-plane': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'a-camera': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'a-sphere': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
     }
   }
   
